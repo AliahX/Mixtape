e598c8e78c9c500a1edb699069773498f2c60ebf
UnJust1ce <tanebosman@gmail.com>
2022-08-27 16:30:22 +1200

    Register new resourcepacks

--- src/main/java/com/unjust1ce/mixtape/config/MixtapePacks.java
+++ src/main/java/com/unjust1ce/mixtape/config/MixtapePacks.java
@@ -12,6 +12,9 @@ public class MixtapePacks {
         Identifier noCavesAndCliffs = new Identifier("mixtape", "no_caves_and_cliffs");
         Identifier noCavesAndCliffsPlusUnusedMusic = new Identifier("mixtape", "no_caves_and_cliffs_plus_unused_music");
         Identifier unusedMusic = new Identifier("mixtape", "unused_music");
+        Identifier end0x10c = new Identifier("mixtape", "0x10c_in_the_end");
+        Identifier noNetherUpdate = new Identifier("mixtape", "no_nether_update");
+        Identifier noNetherUpdatePlusUnusedMusic = new Identifier("mixtape", "no_nether_update_plus_unused_music");
         FabricLoader.getInstance().getModContainer("mixtape").ifPresent(
                 container -> ResourceManagerHelper.registerBuiltinResourcePack(cassettes, container, ResourcePackActivationType.NORMAL));
         FabricLoader.getInstance().getModContainer("mixtape").ifPresent(
@@ -20,5 +23,11 @@ public class MixtapePacks {
                 container -> ResourceManagerHelper.registerBuiltinResourcePack(noCavesAndCliffsPlusUnusedMusic, container, ResourcePackActivationType.NORMAL));
         FabricLoader.getInstance().getModContainer("mixtape").ifPresent(
                 container -> ResourceManagerHelper.registerBuiltinResourcePack(unusedMusic, container, ResourcePackActivationType.NORMAL));
+        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(
+                container -> ResourceManagerHelper.registerBuiltinResourcePack(end0x10c, container, ResourcePackActivationType.NORMAL));
+        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(
+                container -> ResourceManagerHelper.registerBuiltinResourcePack(noNetherUpdate, container, ResourcePackActivationType.NORMAL));
+        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(
+                container -> ResourceManagerHelper.registerBuiltinResourcePack(noNetherUpdatePlusUnusedMusic, container, ResourcePackActivationType.NORMAL));
     }
 }
