1c30afe99aa3edd9ecca18a36290063a1370ef24
Tane Bosman <40045439+UnJust1ce@users.noreply.github.com>
2022-09-25 03:12:30 +1300

    Show if music is paused

--- src/main/java/com/unjust1ce/mixtape/mixin/DebugHudMixin.java
+++ src/main/java/com/unjust1ce/mixtape/mixin/DebugHudMixin.java
@@ -14,6 +14,7 @@ public class DebugHudMixin {
     @Inject(at = @At("RETURN"), method = "getLeftText")
     protected void getLeftText(CallbackInfoReturnable<List<String>> info) {
         info.getReturnValue().add("[Mixtape] Music: " + Mixtape.debugCurrentMusicType);
+        info.getReturnValue().add("[Mixtape] Paused: " + Mixtape.paused);
         info.getReturnValue().add("[Mixtape] Next music type: " + Mixtape.debugNextMusicType);
         info.getReturnValue().add("[Mixtape] Time until next song: " + Mixtape.debugTimeUntilNextSong + "/" + Mixtape.debugMaxTimeUntilNextSong);
     }
