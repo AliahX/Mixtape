291a437f3b165720e884efa3587e1d0c8c7c8300
AliahX <40045439+AliahX@users.noreply.github.com>
2023-04-28 13:48:29 +1200

    Make debug info only show when mod is enabled

--- src/main/java/com/aliahx/mixtape/mixin/DebugHudMixin.java
+++ src/main/java/com/aliahx/mixtape/mixin/DebugHudMixin.java
@@ -1,6 +1,8 @@
 package com.aliahx.mixtape.mixin;
 
 import com.aliahx.mixtape.Mixtape;
+import com.aliahx.mixtape.config.ModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,11 +15,15 @@ import java.util.List;
 public class DebugHudMixin {
     @Inject(at = @At("RETURN"), method = "getLeftText")
     protected void getLeftText(CallbackInfoReturnable<List<String>> info) {
-        info.getReturnValue().add("[Mixtape] Music: " + Mixtape.debugCurrentMusicType);
-        info.getReturnValue().add("[Mixtape] Paused: " + Mixtape.paused);
-        info.getReturnValue().add("[Mixtape] Next music type: " + Mixtape.debugNextMusicType);
-        info.getReturnValue().add("[Mixtape] Time until next song: " + Mixtape.debugTimeUntilNextSong + "/" + Mixtape.debugMaxTimeUntilNextSong);
-        info.getReturnValue().add("[Mixtape] Is disc playing: " + Mixtape.discPlaying);
-        info.getReturnValue().add("[Mixtape] Music volume scale: " + Mixtape.volumeScale);
+        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
+        if(config.mainConfig.enabled) {
+            info.getReturnValue().add("[Mixtape] Music: " + Mixtape.debugCurrentMusicType);
+            info.getReturnValue().add("[Mixtape] Paused: " + Mixtape.paused);
+            info.getReturnValue().add("[Mixtape] Next music type: " + Mixtape.debugNextMusicType);
+            info.getReturnValue().add("[Mixtape] Time until next song: " + Mixtape.debugTimeUntilNextSong + "/" + Mixtape.debugMaxTimeUntilNextSong);
+            info.getReturnValue().add("[Mixtape] Is disc playing: " + Mixtape.discPlaying);
+            info.getReturnValue().add("[Mixtape] Music volume scale: " + Mixtape.volumeScale);
+            Mixtape.jukeBoxesPlaying.forEach((BlockPos, Boolean) -> info.getReturnValue().add("[Mixtape] " + BlockPos + " " + Boolean));
+        }
     }
 }
