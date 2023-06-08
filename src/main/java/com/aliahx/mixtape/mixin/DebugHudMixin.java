9ee6b4a100dc3799797bb70a3c780d721d3fb761
AliahX <40045439+AliahX@users.noreply.github.com>
2023-06-08 13:41:56 +1200

    Update DebugHudMixin.java

--- src/main/java/com/aliahx/mixtape/mixin/DebugHudMixin.java
+++ src/main/java/com/aliahx/mixtape/mixin/DebugHudMixin.java
@@ -1,8 +1,6 @@
 package com.aliahx.mixtape.mixin;
 
 import com.aliahx.mixtape.Mixtape;
-import com.aliahx.mixtape.config.ModConfig;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.hud.DebugHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,19 +9,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
+import static com.aliahx.mixtape.Mixtape.config;
+
 @Mixin(DebugHud.class)
 public class DebugHudMixin {
     @Inject(at = @At("RETURN"), method = "getLeftText")
     protected void getLeftText(CallbackInfoReturnable<List<String>> info) {
-        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-        if(config.mainConfig.enabled) {
-            info.getReturnValue().add("[Mixtape] Music: " + Mixtape.debugCurrentMusicType);
+        if(config.mainConfig.enabled && config.mainConfig.enableDebugInfo) {
+            info.getReturnValue().add("[Mixtape] Song: " + Mixtape.debugCurrentSong);
+            info.getReturnValue().add("[Mixtape] Music Type: " + Mixtape.debugCurrentMusicType);
             info.getReturnValue().add("[Mixtape] Paused: " + Mixtape.paused);
             info.getReturnValue().add("[Mixtape] Next music type: " + Mixtape.debugNextMusicType);
             info.getReturnValue().add("[Mixtape] Time until next song: " + Mixtape.debugTimeUntilNextSong + "/" + Mixtape.debugMaxTimeUntilNextSong);
             info.getReturnValue().add("[Mixtape] Is disc playing: " + Mixtape.discPlaying);
             info.getReturnValue().add("[Mixtape] Music volume scale: " + Mixtape.volumeScale);
-            Mixtape.jukeBoxesPlaying.forEach((BlockPos, Boolean) -> info.getReturnValue().add("[Mixtape] " + BlockPos + " " + Boolean));
+            Mixtape.jukeboxesPlaying.forEach((BlockPos, Boolean) -> info.getReturnValue().add("[Mixtape] " + BlockPos + " " + Boolean));
         }
     }
 }
