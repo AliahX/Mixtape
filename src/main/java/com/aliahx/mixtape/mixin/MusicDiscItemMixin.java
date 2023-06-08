f56e5567ad9f152625aa1641fc7f20ae400ee6cf
AliahX <40045439+AliahX@users.noreply.github.com>
2023-06-08 13:48:38 +1200

    Add droopy likes your face disc

--- src/main/java/com/aliahx/mixtape/mixin/MusicDiscItemMixin.java
+++ src/main/java/com/aliahx/mixtape/mixin/MusicDiscItemMixin.java
@@ -1,7 +1,5 @@
 package com.aliahx.mixtape.mixin;
 
-import com.aliahx.mixtape.config.ModConfig;
-import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.item.MusicDiscItem;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -11,6 +9,8 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.aliahx.mixtape.Mixtape.config;
+
 @Mixin(MusicDiscItem.class)
 public abstract class MusicDiscItemMixin {
 
@@ -18,12 +18,13 @@ public abstract class MusicDiscItemMixin {
 
     @Inject(method = "Lnet/minecraft/item/MusicDiscItem;getDescription()Lnet/minecraft/text/MutableText;", at = @At("HEAD"), cancellable = true)
     private void getDescriptionMixin(CallbackInfoReturnable<MutableText> cir) {
-        ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
         if(config.mainConfig.enabled) {
-            if (this.getComparatorOutput() == 2 && config.jukeboxConfig.dogReplacesCat) {
+            if(this.getComparatorOutput() == 2 && config.jukeboxConfig.dogReplacesCat) {
                 cir.setReturnValue(Text.literal("C418 - dog"));
-            } else if (this.getComparatorOutput() == 11 && config.jukeboxConfig.elevenReplaces11) {
+            } else if(this.getComparatorOutput() == 11 && config.jukeboxConfig.elevenReplaces11) {
                 cir.setReturnValue(Text.literal("C418 - eleven"));
+            } else if(this.getComparatorOutput() == 10 && config.jukeboxConfig.droopyLikesYourFaceReplacesWard) {
+                cir.setReturnValue(Text.literal("C418 - droopy likes your face"));
             }
         }
     }
