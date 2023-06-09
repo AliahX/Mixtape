2e094848f1116d62b591afa05a1e00fddb4b3173
AliahX <40045439+AliahX@users.noreply.github.com>
2023-06-09 13:41:07 +1200

    Renamed no caves & cliffs resourcepack to only c418

--- src/main/java/com/aliahx/mixtape/MixtapePacks.java
+++ src/main/java/com/aliahx/mixtape/MixtapePacks.java
@@ -7,16 +7,16 @@ import net.minecraft.util.Identifier;
 
 public class MixtapePacks {
     public static void init() {
-        Identifier noCavesAndCliffs = new Identifier("mixtape", "no_caves_and_cliffs");
-        Identifier noCavesAndCliffsPlusUnusedMusic = new Identifier("mixtape", "no_caves_and_cliffs_plus_unused_music");
+        Identifier onlyC418 = new Identifier("mixtape", "only_c418");
+        Identifier onlyC418PlusUnusedMusic = new Identifier("mixtape", "only_c418_plus_unused_music");
         Identifier noNetherUpdate = new Identifier("mixtape", "no_nether_update");
         Identifier noNetherUpdatePlusUnusedMusic = new Identifier("mixtape", "no_nether_update_plus_unused_music");
         Identifier unusedMusic = new Identifier("mixtape", "unused_music");
         Identifier end0x10c = new Identifier("mixtape", "0x10c_in_the_end");
         Identifier noBiomeBased = new Identifier("mixtape", "no_biome_based");
         Identifier noBiomeBasedPlusUnusedMusic = new Identifier("mixtape", "no_biome_based_plus_unused_music");
-        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(noCavesAndCliffs, container, ResourcePackActivationType.NORMAL));
-        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(noCavesAndCliffsPlusUnusedMusic, container, ResourcePackActivationType.NORMAL));
+        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(onlyC418, container, ResourcePackActivationType.NORMAL));
+        FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(onlyC418PlusUnusedMusic, container, ResourcePackActivationType.NORMAL));
         FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(noNetherUpdate, container, ResourcePackActivationType.NORMAL));
         FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(noNetherUpdatePlusUnusedMusic, container, ResourcePackActivationType.NORMAL));
         FabricLoader.getInstance().getModContainer("mixtape").ifPresent(container -> ResourceManagerHelper.registerBuiltinResourcePack(unusedMusic, container, ResourcePackActivationType.NORMAL));
