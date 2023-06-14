7a40837eb16bf0d97b92bb5b2ee708826a428002
AliahX <40045439+AliahX@users.noreply.github.com>
2023-06-14 14:40:46 +1200

    Fix version number lol

--- src/main/java/com/aliahx/mixtape/Mixtape.java
+++ src/main/java/com/aliahx/mixtape/Mixtape.java
@@ -43,7 +43,7 @@ import static net.minecraft.sound.SoundCategory.MUSIC;
 public class Mixtape implements ClientModInitializer {
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public static final String MOD_VERSION = "1.5.3";
+    public static final String MOD_VERSION = "1.5.2";
     public static final String MOD_ID = "mixtape";
     private static KeyBinding skipKey;
     private static KeyBinding pauseKey;
