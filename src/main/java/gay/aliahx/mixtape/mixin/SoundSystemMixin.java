172cc93bdb4299f7f4fb2a862a58bc3132f1e137
AliahX <aliahbosman@gmail.com>
2024-01-30 21:26:30 +1300

    Stop noteblocks from playing at 0, 0 when mono is enabled

--- src/main/java/gay/aliahx/mixtape/mixin/SoundSystemMixin.java
+++ src/main/java/gay/aliahx/mixtape/mixin/SoundSystemMixin.java
@@ -1,11 +1,13 @@
 package gay.aliahx.mixtape.mixin;
 
 import gay.aliahx.mixtape.Mixtape;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.sound.*;
 import net.minecraft.sound.SoundCategory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -23,27 +25,32 @@ public abstract class SoundSystemMixin {
     @Shadow private boolean started;
     @Shadow @Final private SoundListener listener;
 
+    @Unique
+    private boolean isNoteBlock(SoundInstance instance) {
+        return instance.getSound().getIdentifier().getPath().contains("note/");
+    }
+
     @Redirect(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundInstance;getX()D"))
     private double getXMixin(SoundInstance instance) {
-        if(!config.main.enabled) return instance.getX();
+        if(!config.main.enabled || isNoteBlock(instance)) return instance.getX();
         return instance.getCategory() == SoundCategory.RECORDS && config.jukebox.mono ? 0 : instance.getX();
     }
 
     @Redirect(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundInstance;getY()D"))
     private double getYMixin(SoundInstance instance) {
-        if(!config.main.enabled) return instance.getY();
+        if(!config.main.enabled || isNoteBlock(instance)) return instance.getY();
         return instance.getCategory() == SoundCategory.RECORDS && config.jukebox.mono ? 0 : instance.getY();
     }
 
     @Redirect(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/sound/SoundInstance;getZ()D"))
     private double getZMixin(SoundInstance instance) {
-        if(!config.main.enabled) return instance.getZ();
+        if(!config.main.enabled || isNoteBlock(instance)) return instance.getZ();
         return instance.getCategory() == SoundCategory.RECORDS && config.jukebox.mono ? 0 : instance.getZ();
     }
 
     @Inject(at = @At("HEAD"), method = "updateSoundVolume", cancellable = true)
     private void updateSoundVolume(SoundCategory category, float volume, CallbackInfo ci) {
-        if(config.main.enabled) {
+        if(config.main.enabled && this.started) {
             this.sources.forEach((source, sourceManager) -> {
                 if (source.getCategory() == SoundCategory.RECORDS) {
                     double distanceToPlayer = Math.sqrt(listener.getPos().squaredDistanceTo(source.getX(), source.getY(), source.getZ()));
